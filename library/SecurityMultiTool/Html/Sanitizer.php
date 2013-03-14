b80fbfbba696b5163669f764df4a8442a2534434
Pádraic Brady <padraic.brady@gmail.com>
2013-03-14 20:07:21 +0000

    Finish minimum HTMLPurifier abstraction class (could as easily just use it directly!)

--- library/SecurityMultiTool/Html/Sanitizer.php
+++ library/SecurityMultiTool/Html/Sanitizer.php
@@ -8,28 +8,55 @@ use SecurityMultiTool\Common
 class Sanitizer extends Common\AbstractOptions implements Common\OptionsInterface
 {
 
-    protected $htmlpurifier = null;
+    protected $purifier = null;
 
-    protected $filter = '';
+    protected $config = null;
 
-    public function filter($html, $filter = '')
+    public function __construct($cachePath, array $options = null)
     {
+        if (!isset($cachePath) || !is_dir($cachePath || !is_writable($cachePath))) {
+            throw new Exception\RuntimeException(
+                'The HTMLPurifier HTML Sanitiser requires a cache location to '
+                . 'improve performance. Please set a cache path or set the '
+                . 'first parameter to the constructor of this class to false. '
+                . 'Ensure the given location, if set, is writable by PHP'
+            );
+        }
+        $this->config = \HTMLPurifier_Config::createDefault();
+        if (false === $cachePath) {
+            $this->getConfig()->set('Core.DefinitionCache', null);
+        } else {
+            $this->getConfig()->set('Cache.SerializerPath', rtrim($cachePath, '\\/ '));
+        }
+        parent::__construct($options);
+    }
 
+    public function sanitize($html, $filter = '')
+    {
+        if (!isset($this->purifier)) {
+            $this->purifier = new \HTMLPurifier($this->getConfig());
+        }
+        return $this->purifier->purify($html, $filter);
+    }
+
+    public function reset()
+    {
+        unset($this->purifier);
     }
 
     public function setOption($key, $value)
     {
-        switch ($key) {
-            case 'value':
-                # code...
-                break;
-            
-            default:
-                throw new Exception\InvalidArgumentException(
-                    'Attempted to set invalid option: ' . $key
-                );
-                break;
-        }
+        $this->getConfig()->set($key, $value);
+    }
+
+    public function getOption($key)
+    {
+        return $this->getConfig()->get($key);
+    }
+
+    public function getConfig()
+    {
+        return $this->config;
     }
 
 }
