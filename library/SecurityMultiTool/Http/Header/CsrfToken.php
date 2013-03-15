66a1c1765437f7d67492adaf35901771831b5543
Pádraic Brady <padraic.brady@gmail.com>
2013-03-15 21:16:29 +0000

    Add Http/CsrfToken header type supporting X-CSRFToken header as alternative to $_POST/$_GET inclusion

---
+++ library/SecurityMultiTool/Http/Header/CsrfToken.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace SecurityMultiTool\Http\Header;
+
+use SecurityMultiTool\Exception;
+
+class CsrfToken extends AbstractHeader implements HeaderInterface
+{
+
+    protected $options = array();
+
+    public function getHeader()
+    {
+        $header = 'X-CSRFToken: ' . $this->getOption('token');
+        return $header;
+    }
+
+    public function send($replace = false)
+    {
+        header($this->getHeader(), $replace);
+    }
+
+    public function setOption($key, $value)
+    {
+        switch ($key) {
+            case 'token':
+                $this->options['token'] = (string) $value;
+                break;
+            
+            default:
+                throw new Exception\InvalidArgumentException(
+                    'Attempted to set invalid option: ' . $key
+                );
+                break;
+        }
+    }
+
+    public function __toString()
+    {
+        return $this->getHeader();
+    }
+
+}
