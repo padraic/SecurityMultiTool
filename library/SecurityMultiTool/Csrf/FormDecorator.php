b7d59e5ca327fee7026d3f935806c476084021c6
Pádraic Brady <padraic.brady@gmail.com>
2013-03-16 12:49:07 +0000

    Add CSRF FormDecorator and Provider protos

---
+++ library/SecurityMultiTool/Csrf/FormDecorator.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace SecurityMultiTool\Csrf;
+
+use SecurityMultiTool\Html\Escaper;
+use SecurityMultiTool\Exception;
+
+class FormDecorator
+{
+
+    protected $provider = null;
+
+    protected $escaper = null;
+
+    public function __construct(Csrf\Provider $provider, $encoding = 'utf-8')
+    {
+        $this->provider = $provider;
+        $this->escaper = new Escaper($encoding);
+    }
+
+    public function decorate($form)
+    {
+        preg_match_all("/<form(.*?)>(.*?)<\\/form>/is", $form, $matches);
+        if (is_array($matches)) {
+            foreach ($matches as $match) {
+                $token = $this->escaper->escapeHtml(
+                    $this->provider->getToken()
+                );
+                $form = str_replace(
+                    $match[0],
+                    '<form' . $match[1] . '>'
+                        . '<input type="hidden" name="CSRFToken" value="'
+                        . $token
+                        . '" />'
+                        . $match[2]
+                        . '</form>',
+                    $form
+                );
+            }
+        } else {
+            throw new Exception\RuntimeException(
+                'Unable to decorate as the given argument does not appear to '
+                'contain valid HTML form markup'
+            );
+        }
+        return $form;
+    }
+
+}
---
+++ library/SecurityMultiTool/Csrf/Provider.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace SecurityMultiTool\Csrf;
+
+use SecurityMultiTool\String\FixedTimeComparison;
+use SecurityMultiTool\Exception;
+
+class Provider extends Common\AbstractOptions implements Common\OptionsInterface
+{
+
+    protected $generator = null;
+
+    protected $token = '';
+
+    protected $tokenNamePrefix = 'CSRFToken';
+
+    protected $name = '';
+
+    protected $timeout = 3600;
+
+    public function __construct(array $options = null)
+    {
+        parent:__construct($options);
+        $this->generator = new Csrf\TokenGenerator;
+    }
+
+    public function getToken($refresh = false)
+    {
+        if (!empty($this->token) && false === $refresh) {
+            return $this->token;
+        }
+        $this->token = $this->generator->generate();
+        $this->storeTokenToSession();
+        return $this->token;
+    }
+
+    public function getTokenName()
+    {
+        return implode(':', array($this->tokenNamePrefix, $this->name));
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getTimeout()
+    {
+        return $timeout;
+    }
+
+    public function isValid($token, $tokenName = null)
+    {
+        if (is_null($tokenName)) {
+            $tokenName = $this->getTokenName();
+        }
+        try {
+            $array = $this->retrieveTokenFromSession($tokenName);
+        } catch (Exception\RuntimeException $e) {
+            return false;
+        }
+        if (empty($array) || !is_array($array) || !isset($array['token'])
+        || !isset($array['expire'])) {
+            return false;
+        }
+        $time = time();
+        if ((int) $array['expire'] >= $time) {
+            return false;
+        }
+        $result = FixedTimeComparison::compare($token, $array['token']);
+        return $result;
+    }
+
+    protected function storeTokenToSession()
+    {
+        if (!session_id()) {
+            session_start();
+        }
+        $expire = 0;
+        if ($this->getTimeout() > 0) {
+            $expire = time() + $this->getTimeout();
+        }
+        $_SESSION[$this->getTokenName()] = array(
+            'token' => $this->getToken(),
+            'expire' => $expire;
+        );
+    }
+
+    protected function retrieveTokenFromSession($tokenName)
+    {
+        if (!session_id()) {
+            throw new Exception\RuntimeException(
+                'A PHP Session has not been started so session storage is '
+                . 'unavailable'
+            );
+        }
+        if (!isset($_SESSION[$tokenName])) {
+            throw new Exception\RuntimeException(
+                'Session data does not include a token for the current token '
+                . 'name: ' . $tokenName
+            );
+        }
+        return $_SESSION[$tokenName];
+
+    }
+
+    public function setOption($key, $value)
+    {
+        switch ($key) {
+            case 'timeout':
+                $this->timeout = (int) $value;
+                break;
+
+            case 'name':
+                $this->name = (string) $value;
+                break;
+
+            case 'token_name_prefix':
+                $this->tokenNamePrefix = (string) $value;
+                break;
+            
+            default:
+                throw new Exception\InvalidArgumentException(
+                    'Attempted to set invalid option: ' . $key
+                );
+                break;
+        }
+    }
+
+}
